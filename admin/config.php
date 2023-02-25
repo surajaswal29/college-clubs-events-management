68db6604b133ff9034c273a2e886ba9b48854ce5
surajaswal29 <surajaswal29@gmail.com>
2023-02-25 10:54:45 +0530

    New Changes

--- admin/config.php
+++ admin/config.php
@@ -1,9 +1,20 @@
 <?php
 
  $host = "localhost";
- $username = "root";
- $password = "";
- $db_name = "hnb-council";
+ 
+
+ if($_SERVER['SERVER_NAME'] === 'localhost'){
+   $username = "root";
+   $password = "";
+   $db_name = "hnb-council";
+
+   echo "We are here";
+ }else{
+   $username = "u932881995_hnbgucc";
+   $password = "Kyojin@12345";
+   $db_name = "u932881995_hnbguclubs";
+ }
+ 
  
  $conn = mysqli_connect($host,$username,$password,$db_name);
 
@@ -12,6 +23,7 @@
  }
 
 
+//page redirection 
  function redirect($loc){
 ?>
 <script>
